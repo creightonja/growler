23e64eb47d89fe269c6f42cc942afda5227e8bdd
Steve Smietana <student@epicodus.com>
2015-09-01 10:51:57 -0700

    fix app.php merge conflicts

--- app/app.php
+++ app/app.php
@@ -1,20 +1,13 @@
 <?php
 
     require_once __DIR__."/../vendor/autoload.php";
-<<<<<<< HEAD
-    require_once __DIR__."/../src/Beer.php";
-    require_once __DIR__."/../src/User.php";
-
-    $app = new Silex\Application();
-
-=======
     require_once __DIR__."/../src/Store.php";
     require_once __DIR__."/../src/Beer.php";
     require_once __DIR__."/../src/Review.php";
     require_once __DIR__."/../src/User.php";
 
     $app = new Silex\Application();
->>>>>>> fdf90617ea4e3a9c5969dd093aefc09d29500704
+
     $app['debug']=true;
 
     $server = 'mysql:host=localhost;dbname=growler';
@@ -22,14 +15,8 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-<<<<<<< HEAD
-    $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'
-    ));
-=======
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views')
     );
->>>>>>> fdf90617ea4e3a9c5969dd093aefc09d29500704
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
@@ -94,19 +81,4 @@
     // });
 
     return $app;
-=======
-    //homepage
-    $app->get("/", function() use($app) {
-        return $app['twig']->render('index.html.twig');
-    });
-
-    $app->get("create_user", function() use ($app) {
-        $user = new User($_POST['user_name'], $_POST['preferred_style'], $_POST['region']);
-        $user->save();
-        return $app['twig']->render('user.html.twig', array('user' => $user));
-    });
-
-    return $app;
-
->>>>>>> fdf90617ea4e3a9c5969dd093aefc09d29500704
 ?>
